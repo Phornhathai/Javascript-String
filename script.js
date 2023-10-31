2a4fa535cdb23316c895ffbce75ea1c2a20f8117
Phornhathai <phornhathai.aum@gmail.com>
2023-10-31 17:09:11 +0700

    add trim and tolowercase

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let bestColor = "purple"
 let quote = 'You had me at "hello"'
 
+// const animal = "hippopotamus"
 console.log(bestColor + ' ' + quote);
+// console.log(animal[7]);
+
+let whisper = "   I love u   "
+console.log(whisper.trim().toLocaleLowerCase());
